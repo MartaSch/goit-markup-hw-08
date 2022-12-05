32037c72e4c427c7c19205ef3198afb6ae3f4bed
MartaSch <martaschab27@gmail.com>
2022-12-05 20:21:27 +0100

    mobile version

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 const menuOverlay = document.querySelector('.mobile-menu');
 const closeMenuButton = document.querySelector('.mobile-menu__toggle');
 const openMenuBtn = document.querySelector('.menu-button');
-const blockmenuOverlay = document.querySelector('.body');
+
 
 openMenuBtn.addEventListener('click', e => {
   menuOverlay.classList.remove('d-none');
@@ -11,9 +11,3 @@ closeMenuButton.addEventListener('click', e => {
   menuOverlay.classList.add('d-none');
 });
 
-openMenuBtn.addEventListener('click', e => {
-    blockmenuOverlay.classList.add('block');
-  });
-  closeMenuButton.addEventListener('click', e => {
-    blockmenuOverlay.classList.remove('block');
-  });
